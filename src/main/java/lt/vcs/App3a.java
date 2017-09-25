e8477bd416ded21a4529991917a61fcd4af9e4c8
Marius Merkevicius <marius.merkevicius@wahanda.com>
2017-09-25 20:05:50 +0300

    3a

--- src/main/java/lt/vcs/App3a.java
+++ src/main/java/lt/vcs/App3a.java
@@ -1,8 +1,17 @@
 package lt.vcs;
 
+/**
+ * 3a. Turim masyvą skaičių [4, 5, 7, 11, 20, 11, 50, 55].
+ * Programa turi išvesti šių skaičių sumą.
+ */
 public class App3a {
 
     public static void main(String[] args) {
-        // new code
+        int[] numbers = {4, 5, 7, 11, 20, 11, 50, 55};
+        int total = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            total += numbers[i];
+        }
+        System.out.println("Total: " + total);
     }
 }
