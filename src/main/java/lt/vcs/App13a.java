88fd9a6d97b51d4071cd25013fca98bf2027c17e
Marius Merkevicius <marius.merkevicius@wahanda.com>
2017-10-05 20:31:12 +0300

    Adding names file

---
+++ src/main/java/lt/vcs/App13a.java
@@ -0,0 +1,14 @@
+package lt.vcs;
+
+/**
+ * 13a. Faile kiekvienoje eilutėje surašytas žmogaus vardas ir amžius.
+ * Parašyti programą kuri nuskaitytų duomenis į HashMap tipo
+ * duomenų struktūrą, tuomet leistų vartotojui įvesti norimą vardą,
+ * ir jei šis buvo duomenų faile, atvaizduoti to žmogaus amžių.
+ */
+public class App13a {
+    public static void main(String[] args) {
+
+
+    }
+}
