c5ea4f0b2a779657ef5be6d1bfb5976a2a0e6094
Marius Merkevicius <asdc@gmail.com>
2017-10-06 20:15:05 +0300

    Printing letters

--- src/main/java/hangman/App.java
+++ src/main/java/hangman/App.java
@@ -5,8 +5,12 @@ import lt.vcs.MyFileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
+
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
         MyFileReader fileReader = new MyFileReader();
         ArrayList<String> lines = fileReader.read("words.txt");
@@ -17,7 +21,11 @@ public class App {
         WordPicker wordPicker = new WordPicker(lines);
         GameLogic gameLogic = new GameLogic(wordPicker.pick(randomNumber));
 
-        System.out.println("Our word: " + gameLogic.validWord);
-
+        String inputLetter;
+        do {
+            inputLetter = scanner.nextLine();
+            gameLogic.displayLetters();
+            gameLogic.checkLetter(inputLetter);
+        } while (!"exit".equals(inputLetter));
     }
 }
--- src/main/java/hangman/GameLogic.java
+++ src/main/java/hangman/GameLogic.java
@@ -8,4 +8,23 @@ public class GameLogic {
         this.validWord = validWord;
     }
 
+    public void checkLetter(String letter) {
+        if (letter.length() > 1) {
+            System.out.println("Dude, only one letter");
+            return;
+        }
+        if (letter.length() == 0) {
+            System.out.println("ONLY ONE LETTER!!!!1");
+            return;
+        }
+        // logic
+    }
+
+    public void displayLetters() {
+        for (int i = 0; i < validWord.length(); i++) {
+            System.out.print("_ ");
+        }
+        System.out.println("");
+    }
+
 }
